d79aaea044ea729d7c88a40b457180559230f282
William Schultz <william.schultz@mongodb.com>
2019-01-16 19:10:57 -0500

    Update monitoring script

--- monitor.py
+++ monitor.py
@@ -1,8 +1,12 @@
 import pymongo
 
-hostname="10.2.0.200"
-port=27017
-replset = "rs0"
-client = MongoClient(hostname, port, replicaset=replset)
-res = client.admin.command("replSetGetStatus")
-print res
+hostnames=["10.2.0.200", "10.2.0.201", "10.2.0.202"]
+
+for hostname in hostnames:
+	port=27017
+	replset = "rs0"
+	client = MongoClient(hostname, port, replicaset=replset)
+	res = client.admin.command("replSetGetStatus")["stateStr"]
+	members = res["members"]
+	member = [m for m in members if "self" in m][0]
+	print hostname, member['stateStr']
