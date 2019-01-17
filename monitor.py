d12107f4c496f9c4d9ee477c6d0df9a1d5484cbf
William Schultz <william.schultz@mongodb.com>
2019-01-16 19:32:48 -0500

    fix

--- monitor.py
+++ monitor.py
@@ -6,11 +6,11 @@ hostnames=["10.2.0.200", "10.2.0.201", "10.2.0.202"]
 for hostname in hostnames:
 	port=27017
 	replset = "rs0"
-    try:
-        client = MongoClient(hostname, port, replicaset=replset, connectTimeoutMS=2000, serverSelectionTimeoutMS=1000)
-        res = client.admin.command("replSetGetStatus")
-        members = res["members"]
-        member = [m for m in members if hostname in m["name"]][0]
-        print hostname, member['stateStr']
-    except:
-        print hostname, "unreachable"
+	try:
+		client = MongoClient(hostname, port, replicaset=replset, connectTimeoutMS=2000, serverSelectionTimeoutMS=1000)
+		res = client.admin.command("replSetGetStatus")
+		members = res["members"]
+		member = [m for m in members if hostname in m["name"]][0]
+		print hostname, member['stateStr']
+	except:
+		print hostname, "unreachable"
