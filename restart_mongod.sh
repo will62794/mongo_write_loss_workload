33ca8b4f86e128b51ee0f7446056678c204948e3
William Schultz <william.schultz@mongodb.com>
2019-01-16 19:16:34 -0500

    fix

--- restart_mongod.sh
+++ restart_mongod.sh
@@ -3,9 +3,6 @@
 #
 # Continuously kill and restart local mongod processes.
 #
-# e.g. Restart nodes every 30 seconds:
-#
-# 	./restart_mongod.sh 30
 #
 
 
@@ -20,7 +17,7 @@ do
 	echo "Restarting mongod..."
 	mongodb/bin/mongod --config /tmp/mongo_port_27017.conf
 	# Sleep a bit before next restart.
-	sleep_secs=`./rand.py`
+	sleep_secs=`./randexp.py`
 	echo $sleep_secs
 	echo "Sleeping for ${sleep_secs} seconds before next restart"
 	echo "---------"
