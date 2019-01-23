4286ec15c49b31f94b6400fcdddc031ca00d42bd
William Schultz <william.schultz@mongodb.com>
2019-01-23 16:36:25 -0500

    updates

--- continuous_restart.sh
+++ continuous_restart.sh
@@ -23,17 +23,16 @@ randexp(){
 # Continuously kill and restart mongod processes running locally.
 while true
 do
+	sleep_before_restart_secs="10"
 	echo "Killing mongod on `hostname`"
 	killall -9 mongod
 
-	sleep_before_restart_secs="10"
 	echo "Restarting mongod in $sleep_before_restart_secs seconds."
 	sleep $sleep_before_restart_secs
 	mongodb/bin/mongod --config /tmp/mongo_port_27017.conf
-	echo "Restarted mongod."
 
 	# Sleep a bit before next restart.
 	sleep_secs=`randexp`
-	echo "Sleeping for ${sleep_secs} seconds before next restart"
+	echo "Restarted mongod. Sleeping for ${sleep_secs} seconds before next restart"
 	sleep $sleep_secs
 done
