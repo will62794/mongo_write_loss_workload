816fe2635b08e8b7b286768bbe12c5246160d17d
William Schultz <william.schultz@mongodb.com>
2019-01-28 17:33:30 -0500

    Update workload runner with more parameters

--- run_workload.sh
+++ run_workload.sh
@@ -7,25 +7,40 @@
 
 set -e
 
+
 restart_interval_mean_secs="80"
 numDocs="200000000"
-timeLimitSecs="900"
+# timeLimitSecs="3600" # 1 hour
+timeLimitSecs="900" # 15 minutes
 numWorkers="30"
-writeConcern="1"
-logfile="workload_w${writeConcern}.log"
+writeConcern="majority"
+netLatencyMS="100"
+datestr=`date +"%Y_%m_%d_%I_%M_%S"`
+logfile="workload_results/workload_w${writeConcern}_latency_${netLatencyMS}ms_${datestr}.log"
 
+# Make sure Linux traffic control is installed on all nodes.
+python ../bin/conn.py -d md.0 md.1 md.2 -c "sudo yum -y install tc"
+
+#
 # Set up the mongod nodes.
+#
 echo "--- Setting up the mongod nodes. ---"
 git_repo="https://github.com/will62794/mongo_write_loss_workload"
 commands="rm -rf mongo_write_loss_workload"
 commands="$commands && git clone --progress $git_repo"
 commands="$commands && cp mongo_write_loss_workload/continuous_restart.sh ."
 killcmd="pkill -f continuous_restart" # kill previously running scripts.
+removelatencycmd="sudo /usr/sbin/tc qdisc del dev eth0 root netem"
+addlatencycmd="sudo /usr/sbin/tc qdisc add dev eth0 root netem delay ${netLatency}ms"
 runcmd="nohup bash continuous_restart.sh $restart_interval_mean_secs > restarts.log &"
 python ../bin/conn.py -d md.0 md.1 md.2 -c "$killcmd"
+python ../bin/conn.py -d md.0 md.1 md.2 -c "$removelatencycmd"
+python ../bin/conn.py -d md.0 md.1 md.2 -c "$addlatencycmd"
 python ../bin/conn.py -d md.0 md.1 md.2 -c "$commands" -c "$runcmd"
 
+#
 # Set up the workload client.
+#
 echo "--- Setting up the workload client. ---"
 hostname=`grep -m 1 "private_ip" infrastructure_provisioning.out.yml | sed "s/.*private_ip\: //"`
 commands="pip install --user pymongo"
@@ -35,4 +50,14 @@ killcmd="pkill -9 -f failover_workload" # kill previously running workload.
 workloadcmd="nohup python mongo_write_loss_workload/failover_workload.py --host $hostname --port 27017 --replset rs0 --numDocs $numDocs --numWorkers $numWorkers --writeConcern $writeConcern --log $logfile --timeLimitSecs $timeLimitSecs > /dev/null &"
 python ../bin/conn.py wc -c "$killcmd"
 python ../bin/conn.py wc -c "$commands" -c "$workloadcmd"
+
+
+
+# Set 100ms of latency on device 'eth0'
+# sudo tc qdisc add dev eth0 root netem delay 100ms
+
+# Remove tc rule on 'eth0'
+# sudo tc qdisc del dev eth0 root netem
+
+
 
